f0f7a8f4129c959b67d8e67d871feac86a72f2da
JimoYT <71781063+JimoYT@users.noreply.github.com>
2021-10-28 10:34:56 +0100

    Update wireguard-vpn-script.py

--- wireguard-vpn-script.py
+++ wireguard-vpn-script.py
@@ -6,14 +6,14 @@ from requests import get
 ip = get('https://api.ipify.org').text
 
 print('connecting to ssh of the vpn')
-os.system('ssh -l root -p (ssh port) (your vps ip)')
+os.system('ssh -l root -p (port) (your vps ip')
 print('now connecting to vpn')
 os.system('sudo wg-quick up wg0-client')
 print('vpn is now connected!')
 time.sleep(1)
 print('now checking if ip has changed')
 time.sleep(1)
-os.system('My IP is: {}'.format(ip))
+print('My IP is: {}'.format(ip))
 print('double checking if the vpn is actually connected by using wg')
 time.sleep(1)
 os.system('sudo wg')
